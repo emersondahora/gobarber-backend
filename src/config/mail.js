6c6ee8e462b4e286f95734a43449c0592c0e0523
Emerson Oliveira da Hora <emersondahora@gmail.com>
2020-02-06 15:06:57 -0300

    Cancelamento e envio de e-mail + campos adicionais nos agendamentos

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b2cd8dd5b14599',
+    pass: '6a3077eaf34bd2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
